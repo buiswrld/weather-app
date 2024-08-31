fc76882438061d0038b88885243429ce9a20baf8
buiswrld <buiswrld@gmail.com>
2024-08-31 03:35:12 -0700

    add new endpoints for new backend format

--- frontend/src/api/location-service.ts
+++ frontend/src/api/location-service.ts
@@ -1,13 +1,25 @@
-export interface LocationData {
+export interface LocationCoords {
     lat: number;
-    lng: number;
-    place_name: string;
+    lon: number;
 }
 
-export const fetchLocationData = async (location: string): Promise<LocationData> => {
+export interface LocationName {
+    name: string;
+}
+
+export const fetchLocationData = async (location: string): Promise<LocationCoords[]> => {
     const response = await fetch(`http://localhost:5000/api/location?location=${location}`);
     if (!response.ok) {
         throw new Error('Failed to fetch location data');
+    }
+    const data = await response.json()
+    return data;
+}
+
+export const fetchLocationName = async (lat: string, lon: string): Promise<LocationName[]> => {
+    const response = await fetch(`http://localhost:5000/api/coords?lat=${lat}&lon=${lon}`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch location name');
     }
     const data = await response.json();
     return data;
