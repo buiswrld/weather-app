9826e15d1716ea68852d9c1952fc6c0f4d83a439
buiswrld <buiswrld@gmail.com>
2024-08-30 20:19:48 -0700

    update weather service to reflect function name refactoring

--- frontend/src/api/weather-service.ts
+++ frontend/src/api/weather-service.ts
@@ -1,7 +1,7 @@
 import { WeatherData } from './model';
 
 export const fetchTemperature = async(lat: string, lon: string): Promise<WeatherData> => {
-    const response = await fetch(`http://localhost:5000/api/weather?lat=${lat}&lon=${lon}`);
+    const response = await fetch(`http://localhost:5000/api/temperature?lat=${lat}&lon=${lon}`);
     const data = await response.json();
     return data;
 }
