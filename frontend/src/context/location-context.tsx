21bfb0d7ebe7e32bfbd6836c1da8a4cf97d3990e
buiswrld <buiswrld@gmail.com>
2024-09-01 22:14:01 -0700

    documentation

--- frontend/src/context/location-context.tsx
+++ frontend/src/context/location-context.tsx
@@ -1,18 +1,34 @@
 import React, { createContext, useState, useEffect, ReactNode } from 'react';
 import { LocationCoords } from '../api/models/location-model';
 
+/**
+ * Interface for the LocationContext properties.
+ */
 interface LocationContextProps {
   locationData: LocationCoords | null;
   setLocationData: (data: LocationCoords) => void;
 }
 
+/**
+ * Default values for the LocationContext.
+ */
 const defaultLocationContext: LocationContextProps = {
   locationData: null,
   setLocationData: () => {}
 };
 
+/**
+ * Creates a context for location data.
+ */
 export const LocationContext = createContext<LocationContextProps>(defaultLocationContext);
 
+
+/**
+ * LocationProvider component that provides location data to its children.
+ * 
+ * @param children - The child components that will consume the location context.
+ * @returns A provider component that supplies location data and a function to update it.
+ */
 export const LocationProvider = ({ children }: { children: ReactNode }) => {
   const [locationData, setLocationData] = useState<LocationCoords | null>(null);
 
